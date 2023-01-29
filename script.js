356ed4d4b320355c447a7040a7c75751594e2f0c
Jules <115895273+Djulsya@users.noreply.github.com>
2023-01-29 16:19:40 +0300

    до бэм

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-// let popupOpened = content.querySelector('.profile__editbutton_popup_opened');
-// let popupClosed = content.querySelector('.popup__savebutton_popup_closed');
+let popup = document.querySelector ('.popup');
+let buttonEditProfileOpen = document.querySelector ('.profile__editbutton');
+let editForm = document.querySelector ('.popup__container');
+let getName = document.querySelector ('.profile__name');
+let getAbout = document.querySelector ('.profile__about');
+let nameInput = editForm.querySelector ('.popup__editform_name');
+let aboutInput =  editForm.querySelector ('.popup__editform_about');
+let buttonEditProfileSave = document.querySelector ('.popup__savebutton');
+let buttonEditProfileClose = document.querySelector ('.popup__closebutton');
+
+function openPopup () {
+  popup.classList.add ('popup_opened');
+  nameInput.value = getName.textContent;
+  aboutInput.value = getAbout.textContent;
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    getName.textContent = `${nameInput.value}`;
+    getAbout.textContent = `${aboutInput.value}`;
+}
+
+function closePopup ()  {
+  popup.classList.remove ('popup_opened');
+  }
+
+buttonEditProfileOpen.addEventListener ('click', openPopup);
+editForm.addEventListener('submit', formSubmitHandler);
+buttonEditProfileSave.addEventListener ('click', closePopup);
+buttonEditProfileClose.addEventListener ('click', closePopup);
